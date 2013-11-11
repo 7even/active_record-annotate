eb7d27190cc1c8e0da0cfb3ebcf8c602d8079794
Vsevolod Romashov <7@7vn.ru>
2013-11-12 02:38:16 +0400

    AR::Annotate.models

--- lib/active_record/annotate.rb
+++ lib/active_record/annotate.rb
@@ -1,8 +1,26 @@
 require 'active_record/annotate/dumper'
 require 'active_record/annotate/version'
+require 'active_record/annotate/railtie'
 
 module ActiveRecord
   module Annotate
-    require 'active_record/annotate/railtie' if defined?(Rails)
+    class << self
+      def models
+        models_dir = Rails.root.join('app/models')
+        files_mask = models_dir.join('**', '*.rb')
+        
+        Dir.glob(files_mask).each_with_object(Hash.new) do |path, models|
+          # .../app/models/car/hatchback.rb -> car/hatchback
+          short_path = path.sub(models_dir.to_s + '/', '').sub(/\.rb$/, '')
+          # skip any app/models/concerns files
+          next if short_path.starts_with?('concerns')
+          
+          # car/hatchback -> Car::Hatchback
+          klass = short_path.camelize.constantize
+          # collect only AR::Base descendants
+          models[klass.table_name] = path if klass < ActiveRecord::Base
+        end
+      end
+    end
   end
 end
