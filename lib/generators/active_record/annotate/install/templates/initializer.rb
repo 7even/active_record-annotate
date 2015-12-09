18bb6ef479e4e45cf31aa3aa1a0ef9628108cfa1
Vsevolod Romashov <7@7vn.ru>
2015-12-09 12:26:48 +0300

    fix generated annotate.rb file

--- lib/generators/active_record/annotate/install/templates/initializer.rb
+++ lib/generators/active_record/annotate/install/templates/initializer.rb
@@ -4,4 +4,4 @@ ActiveRecord::Annotate.configure do |config|
   # set this to true to wrap annotations in triple backticks (```)
   # so YARD documentation can process the annotation as a code block
   # config.yard = false
-end
+end if ActiveRecord.const_defined?(:Annotate)
